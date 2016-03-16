9bdb8a516dea90e2d84bb5d21ab7aa3ee9913e9b
liuyong <583633876@qq.com>
2016-03-16 17:50:37 +0800

    base

--- Learn/BaseDemo/file.rb
+++ Learn/BaseDemo/file.rb
@@ -1,4 +1,10 @@
 
 class File
-
+  #写文件
+  File.open("E:\\11.txt",'w') do |file|
+    file.puts "test"
+  end
+  File.open("E:\\sdxl.txt","r").each_line do |line|
+    print line
+  end
 end
---
+++ Learn/BaseDemo/mysql.rb
@@ -0,0 +1,9 @@
+require 'mysql'
+class Mysql
+  m = Mysql.new("localhost","root","liuyong","test")
+  r = m.query("select * from user")
+  r.each_hash do |f|
+    print "#{f['username']}"
+  end
+
+end
